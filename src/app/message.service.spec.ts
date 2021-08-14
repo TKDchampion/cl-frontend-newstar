79713213cc15905f16599a0e42cfad096bd72910
TKDchampion <as1219123@yahoo.com.tw>
2021-08-14 20:23:42 +0800

    Angular-s2 add messages service unittest and 清除頁面底部(Messages)裡的前五筆訊息

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -1,15 +1,60 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject } from "@angular/core/testing";
 
-import { MessageService } from './message.service';
+import { MessageService } from "./message.service";
 
-describe('MessageService', () => {
+describe("MessageService", () => {
+  let service: MessageService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [MessageService]
+      providers: [MessageService],
     });
+    service = TestBed.get(MessageService);
   });
 
-  it('should be created', inject([MessageService], (service: MessageService) => {
-    expect(service).toBeTruthy();
-  }));
+  it("should be created", inject(
+    [MessageService],
+    (service: MessageService) => {
+      expect(service).toBeTruthy();
+    }
+  ));
+
+  describe(`'clear' function testing`, () => {
+    it(`when you input messages > 5, should clear first five strokes`, () => {
+      service.messages = FakeData;
+      service.clear();
+      expect(service.messages).toEqual([
+        "HeroService: deleted hero id=15",
+        "HeroService: deleted hero id=16",
+        "HeroService: deleted hero id=17",
+        "HeroService: deleted hero id=18",
+        "HeroService: deleted hero id=19",
+        "HeroService: deleted hero id=20",
+      ]);
+    });
+
+    it(`when you input messages <= 5, should clear all`, () => {
+      service.messages = [
+        "HeroService: fetched heroes",
+        "HeroService: deleted hero id=11",
+        "HeroService: deleted hero id=12",
+      ];
+      service.clear();
+      expect(service.messages).toEqual([]);
+    });
+  });
 });
+
+const FakeData = [
+  "HeroService: fetched heroes",
+  "HeroService: deleted hero id=11",
+  "HeroService: deleted hero id=12",
+  "HeroService: deleted hero id=13",
+  "HeroService: deleted hero id=14",
+  "HeroService: deleted hero id=15",
+  "HeroService: deleted hero id=16",
+  "HeroService: deleted hero id=17",
+  "HeroService: deleted hero id=18",
+  "HeroService: deleted hero id=19",
+  "HeroService: deleted hero id=20",
+];
--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,14 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class MessageService {
   messages: string[] = [];
+  testText = 0;
 
   add(message: string) {
     this.messages.push(message);
   }
 
   clear() {
-    this.messages = [];
+    const index = this.messages.length;
+    index <= 5
+      ? (this.messages = [])
+      : (this.messages = this.messages.slice(5, index));
   }
 }
